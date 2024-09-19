96c265a08e93fbee0dd26db8578150f21c1baf86
Merus23 <mateus.satira@gmail.com>
2024-09-19 20:27:21 -0300

    feat: add the CRUD routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProdutoController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rotas CRUD para o Produto
+Route::get('produtos', [ProdutoController::class, 'index']); // Lista todos os produtos
+Route::get('produtos/{id}', [ProdutoController::class, 'show']); // Busca produto por id
+Route::post('produtos', [ProdutoController::class, 'store']); // Cria um novo produto
+Route::put('produtos/{id}', [ProdutoController::class, 'update']); // Atualiza um produto existente
+Route::delete('produtos/{id}', [ProdutoController::class, 'destroy']); // Deleta um produto
