4244ff56872aebfa3942f63d0a0f51e245c61177
Merus23 <mateus.satira@gmail.com>
2024-09-20 00:09:40 -0300

    fix: fix problem with the properties name

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     protected $primaryKey = 'cod_produto';
-    protected $fillable = ['nome_produto', 'marca_produto', 'cod_cidade', 'preco_produto'];
+    protected $fillable = ['nome_produto', 'valor_produto', 'marca_produto', 'estoque', 'cidade',];
+
+    public function marca()
+    {
+        return $this->belongsTo(Marca::class, 'marca_produto', 'cod_marca');
+    }
 
     public function cidade()
     {
