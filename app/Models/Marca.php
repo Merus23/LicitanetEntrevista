d703778a0e4062603d401baa335d0f29cb743b30
Merus23 <mateus.satira@gmail.com>
2024-09-20 00:11:45 -0300

    feat: add new properties at Marca model

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     protected $primaryKey = 'cod_marca';
-    protected $fillable = ['nome_marca'];
+    protected $fillable = ['nome_marca', 'fabricante'];
     public function produtos()
     {
         return $this->hasMany(Produto::class, 'marca_produto', 'cod_marca');
